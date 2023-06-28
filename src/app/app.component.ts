c9c6de8db2fb4b63f0f8dbb9c91d0150cd589392
Daniel Caro <deco2449584@gmail.com>
2023-06-28 17:47:19 -0500

    ngfor para arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Products } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,6 +19,45 @@ export class AppComponent {
   fondo = 'gray';
   names: string[] = ['Daniel', 'Juan', 'Pedro'];
   newName: string = '';
+  products: Products[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/bike.jpg',
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/album.jpg',
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/books.jpg',
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/house.jpg',
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image:
+        'https://raw.githubusercontent.com/platzi/angular-fundamentals/10-step/src/assets/images/glasses.jpg',
+    },
+  ];
   toggleBtn() {
     this.btn = !this.btn;
   }
