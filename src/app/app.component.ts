ea22f49d669d5f136b2b789f624a3dc7bb6ab22e
Daniel Caro <deco2449584@gmail.com>
2023-06-28 16:57:19 -0500

    fundamentos Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'Daniel';
+  age = 25;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btn = true;
+  person = {
+    name: 'DanielOPbjeto',
+    age: 25,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
+  fondo = 'gray';
+
+  toggleBtn() {
+    this.btn = !this.btn;
+  }
+  aumentar() {
+    this.person.age = this.person.age + 1;
+  }
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
